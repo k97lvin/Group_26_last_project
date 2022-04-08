07c3c4e9b647a2e9dd0ec26ca28163e77dea43ad
elvinkarimli <95429608+k97elvin@users.noreply.github.com>
2022-04-08 01:56:20 -0400

    Commit 2

--- src/test/java/com/trycloud/step_definitions/FilesModule_StepDefinition.java
+++ src/test/java/com/trycloud/step_definitions/FilesModule_StepDefinition.java
@@ -4,12 +4,12 @@ import com.trycloud.pages.FilePage;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.WebElement;
 
 public class FilesModule_StepDefinition {
 
     FilePage filePage = new FilePage();
-
 
 
     @When("the user clicks action-icon  from any ﬁle on the page")
@@ -20,17 +20,20 @@ public class FilesModule_StepDefinition {
     }
 
     @And("user choose the Add to favorites option")
-    public void userChooseTheOption() {
+    public void userChooseTheAddToFavoritesOption() {
         filePage.addToFavoritesIcon.click();
     }
 
     @And("user click the Favorites sub-module on the left side")
-    public void userClickTheSubModuleOnTheLeftSide() {
+    public void userClickTheFavoritesSubModuleOnTheLeftSide() {
         filePage.favoritesListIcon.click();
-
     }
 
     @Then("Verify the chosen ﬁle is listed on the table")
     public void verifyTheChosenﬁleIsListedOnTheTable() {
+        String folderNAme = "New folder";
+        String actual = filePage.folderNameInFavorites.getText();
+        Assert.assertEquals(folderNAme, actual);
     }
+
 }
